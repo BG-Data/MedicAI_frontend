5b0eb64e5f4a46d44035e1c2f54dcab47d48675a
marianogueirac <camargonmaria@gmail.com>
2024-04-18 21:24:07 -0300

    [style] add styles, assests and new dependencies

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, View, Image, TouchableOpacity, Text} from 'react-native';
+import { Input } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import styles from './style/MainStyle';
 
 export default function App() {
+
+  const [email, setEmail] = useState(null)
+  const [password, setpassword] = useState(null)
+
+  // Defina o componente CustomButton
+const CustomButton = ({ title, onPress, style }) => {
+  return (
+    <TouchableOpacity style={[styles.customButton, style]} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+  const entrar = () => {
+    console.log("entrou")
+    console.log(email)
+    console.log(password)
+  } //mostrar no console o que foi digitado nos campos
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Login MedicAI</Text>
+      </View>
+      
+       <Image
+         source={require('./assets/doctorai.png')}
+         style={{ width: 150, height: 150}}
+        />
+
+      <View style={styles.inputContainer}>
+      <Image
+          source={require('./assets/email.png')}
+          style={{ width: 20, height: 20, marginRight: 10 }} // Defina o tamanho do ícone e a margem direita conforme necessário
+      />
+
+      <Input
+        placeholder='Digite seu e-mail'
+        inputStyle={styles.emailPlaceholder}
+        onChangeText={value => setEmail(value)}
+        keyboardType='email-address' //tipo de caixa
+        containerStyle={{ flex: 1 }}
+      />
+      </View>
+
+      <View style={styles.inputContainer1}>
+      <Image
+          source={require('./assets/password.png')}
+          style={{ width: 20, height: 20, marginRight: 10 }} // Defina o tamanho do ícone e a margem direita conforme necessário
+      />
+      <Input
+        placeholder='Digite sua senha'
+        inputStyle={styles.emailPlaceholder}
+
+        onChangeText={value => setpassword(value)}
+        secureTextEntry // Isso oculta o texto digitado para a senha
+      />
+     </View>
+
+      {/* Utilize o componente CustomButton */}
+      <CustomButton
+        title="ENTRAR"
+        onPress={() => entrar()}
+        
+      />
+      
+      <View style={{ marginTop: 10 }}>
+        <Text style={styles.linkText}>
+          Ainda não tem uma conta?{' '}
+          <Text
+            style={styles.link}
+            onPress={() => console.log('Link clicado')} // logica de clique
+          >
+            Clique aqui
+          </Text>
+        </Text>
+
+        <Text style={styles.linkText} onPress={() => console.log('Redefinir senha')}>
+          Esqueci minha senha
+        </Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ style/MainStyle.js
@@ -0,0 +1,105 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#F5F5F5',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    inputContainer1: {
+      backgroundColor: '#ffffff', // Cor de fundo branca para o contêiner do campo de email
+      borderRadius: 15, // Borda arredondada
+      marginBottom: 10, // Margem inferior para espaçamento
+      paddingHorizontal: 15, // Preenchimento horizontal para espaçamento interno
+      width: '90%', // Largura do contêiner (ajuste conforme necessário)
+      flexDirection: 'row', // Alinha itens horizontalmente
+      alignItems: 'center', // Alinha itens verticalmente
+      marginBottom: 15, // Adicione margem inferior conforme necessário
+    },
+
+    inputContainer: {
+      backgroundColor: '#ffffff', // Cor de fundo branca para o contêiner do campo de email
+      borderRadius: 15, // Borda arredondada
+      marginBottom: 10, // Margem inferior para espaçamento
+      paddingHorizontal: 15, // Preenchimento horizontal para espaçamento interno
+      width: '90%', // Largura do contêiner (ajuste conforme necessário)
+      flexDirection: 'row', // Alinha itens horizontalmente
+      alignItems: 'center', // Alinha itens verticalmente
+      marginBottom: 15, // Adicione margem inferior conforme necessário
+    },
+
+
+    emailPlaceholder: {
+      color: '#069E6E', // Cor do texto do placeholder do email
+    },
+
+    header: {
+      backgroundColor: '#069E6E', // Cor de fundo verde para o cabeçalho
+      paddingVertical: 65,
+      width: '100%', // Largura total para o cabeçalho ocupar toda a largura da tela
+      alignItems: 'center',
+      position: 'absolute', // Posicionar absolutamente em relação ao contêiner pai
+      top: 0, // Posicionar no topo do contêiner pai
+    },
+    headerText: {
+      color: '#fff', // Cor do texto do cabeçalho
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+  
+    icon: {
+      marginRight: 10,
+      alignSelf: 'center', // Centraliza verticalmente o ícone
+    },
+
+    linkText: {
+      color: '#069E6E', // Cor do link
+      textAlign: 'center',
+    },
+
+    linkContainer: {
+      marginBottom: 20, // Espaço entre links
+    },
+
+    link: {
+      color: '#069E6E', // Cor do link
+      fontWeight: 'bold',
+    },
+
+    input: {
+      width: '80%',
+      marginBottom: 15,
+    },
+
+    customButton: {
+      backgroundColor: '#069E6E',
+      borderRadius: 15,
+      paddingVertical: 15,
+      paddingHorizontal: 80,
+      borderWidth: 1,
+      borderColor: '#6D6D6D',
+      marginTop: 20,
+    },
+
+    buttonText: {
+      color: '#ffffff',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+
+    apresentacao: {
+      alignItems: 'center', // Centraliza os itens horizontalmente
+      marginBottom: 20, // Espaçamento inferior
+      paddingHorizontal: 20, // Preenchimento horizontal
+    },
+  
+    apresentacaoText: {
+      textAlign: 'center', // Centraliza o texto horizontalmente
+      width: '80%', // Define a largura do texto
+    },
+
+  });
+
+export default styles
