555183d13f9e7e08451b4f890c59e46ee84da348
marianogueirac <camargonmaria@gmail.com>
2024-04-29 20:12:03 -0300

    navegação BottomTab e adição novas telas

--- App.js
+++ App.js
@@ -1,16 +1,54 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, View, Image, TouchableOpacity, Text} from 'react-native';
-import { Input } from 'react-native-elements';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import styles from './style/MainStyle';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Login from './screens/Login';
 import Home from './screens/Home';
 import PrimeiroAcesso from './screens/PrimeiroAcesso';
 import ResetSenha from './screens/ResetSenha';
+import Chat from './screens/Chat';
+import Historico from './screens/Historico';
+import Perfil from './screens/Perfil';
+import Assuntos from './screens/Assuntos';
+import Favoritos from './screens/Favoritos';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home-outline';
+          } else if (route.name === 'Chat') {
+            iconName = 'chat-outline';
+          } else if (route.name === 'Historico') {
+            iconName = 'history';
+          } else if (route.name === 'Perfil') {
+            iconName = 'account-outline';
+          }
+
+          return <MaterialCommunityIcons name={iconName} color={color} size={size} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#069E6E',
+        inactiveTintColor: '#545454',
+      }}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Chat" component={Chat} />
+      <Tab.Screen name="Historico" component={Historico} />
+      <Tab.Screen name="Perfil" component={Perfil} />
+    </Tab.Navigator>
+  );
+}
 
 function MyStack() {
 
@@ -33,8 +71,15 @@ function MyStack() {
           options={{...commonHeaderStyle,  title: 'Primeiro Acesso',}}/>
         <Stack.Screen name="ResetSenha" component={ResetSenha}
           options={{...commonHeaderStyle,  title: 'Recuperar senha',}}/>
-        <Stack.Screen name="Home" component={Home}
-        options={commonHeaderStyle}/>
+        <Stack.Screen name="Home" component={HomeTabs}
+        options={{ headerShown: false }}/>
+
+        <Stack.Screen name="Favoritos" component={Favoritos}
+          options={{...commonHeaderStyle,  title: 'Seus favoritos',}}/>
+
+        <Stack.Screen name="Assuntos" component={Assuntos}
+          options={{...commonHeaderStyle,  title: 'Seus assuntos',}}/>
+
       </Stack.Navigator>
   );
 }
