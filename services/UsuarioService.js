d89c0431dc9af3b58cc5cb52e515fa8681fbd55e
marianogueirac <camargonmaria@gmail.com>
2024-04-30 16:17:53 -0300

    correção para rota de cadastro

--- services/UsuarioService.js
+++ services/UsuarioService.js
@@ -3,12 +3,15 @@ import axios from "axios";
 const UsuarioService = {
   async cadastrar(data) {
     try {
-      console.log("enviando dados para cadastrar usuário:", data);
-      const response = await axios.post("http://3.101.57.70/user/", data);
-      console.log("resposta da API", response.data);
+      const options = {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      };
+      const response = await axios.post("http://3.101.57.70/user/", data,options);
       return response.data;
     } catch (error) {
-      console.error("Erro ao cadastrar usuário:", error);
       throw new Error("Erro ao cadastrar usuário: " + error.message);
     }
   }
